ab74c7dea449c4cfdadcc461edad1d57a10810f5
Tomek Cichon <tomek1149@gmail.com>
2025-02-09 16:02:19 +0100

    test

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import serverless from 'serverless-http'
 
 import { setupVite, serveStatic, log } from "./vite";
 
@@ -68,4 +67,3 @@ app.use((req, res, next) => {
 
 
 module.exports = app;
-module.exports.handler = serverless(app);
